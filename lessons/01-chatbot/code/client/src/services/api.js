b65ba9298dcef8770b728637ed0bca6645894e09
Marty Bradley <marty.agilecoach@gmail.com>
2024-09-28 15:15:12 -0400

    Add client folder content for Lesson 01: Chatbot

---
+++ lessons/01-chatbot/code/client/src/services/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export const sendMessage = async (message) => {
+  try {
+    const response = await axios.post('http://localhost:8000/chat', { message });
+    return response.data.reply;
+  } catch (error) {
+    console.error('Error:', error);
+    return 'Error occurred while chatting.';
+  }
+};
